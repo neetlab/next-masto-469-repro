f2175f4ec6857fc944a34fe7bf18e33af892a6a7
Ryo Igarashi <n33t5hin@gmail.com>
2021-09-11 09:12:54 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
 import { useAsync } from "react-use";
-import { login } from "masto";
+import * as Masto from "masto";
 
 const Timeline = (props) => {
   const { masto } = props;
-  const timeline = useAsync(() => masto.timelines.getTagIterable("mastodon").next());
+  const timeline = useAsync(() =>
+    masto.timelines.getTagIterable("mastodon").next()
+  );
 
   if (timeline.loading) {
     return <span>loading the timeline...</span>;
   }
 
+  console.log(timeline);
+
   return (
     <ul>
-      {timeline.value.flatMap((statuses) =>
-        statuses.map((status) => (
-          <li key={status.id}>
-            <p dangerouslySetInnerHTML={{ __html: status.content }} />
-          </li>
-        ))
-      )}
+      {timeline.value.value.flatMap((status) => (
+        <li key={status.id}>
+          <p dangerouslySetInnerHTML={{ __html: status.content }} />
+        </li>
+      ))}
     </ul>
   );
 };
 
 export default function Home() {
-  const masto = useAsync(() => login({ url: "https://mastodon.social" }));
+  const masto = useAsync(() => Masto.login({ url: "https://mastodon.social" }));
 
   if (masto.loading) {
     return <span>logging into Mastodon...</span>;
